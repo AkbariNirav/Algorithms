83c5d31735566b54da6a6cd19a4ac4ebea564282
Nirav Akbari <AkbariNirav@users.noreply.github.com>
2017-08-20 22:44:00 +0530

    Update and rename PrimeNumber.c to PrimeNumberGenerator.cpp
    Prime number generator using sieve of Eratosthenes

---
+++ PrimeNumberGenerator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+bool sieve(int n)
+{
+	int prime[n+1]={0};
+	for(int i=2;i*i<=n;i++)
+	{
+		if(prime[i]==false)
+		{
+			for(int j=2;j<=n;j++)
+			{
+				if(i*j<=n)
+					prime[i*j]=true;
+				else
+					break;
+			}
+		}
+	}
+	for (int p=2; p<=n; p++)
+       	if (prime[p]==false)
+       		printf("%d ",p);
+}
+
+int main() {
+	int n;
+	scanf("%d",&n);
+	sieve(n);
+	return 0;
+}
