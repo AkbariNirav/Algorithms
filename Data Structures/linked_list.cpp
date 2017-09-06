8111a64d98a395fe8459414e7109ec4fcec85ea7
Nirav Akbari <AkbariNirav@users.noreply.github.com>
2017-09-06 14:17:14 +0530

    Insert at any index added

--- Data Structures/linked_list.cpp
+++ Data Structures/linked_list.cpp
@@ -71,11 +71,43 @@ void insert_middle(int n,int d)
     }
 }
 
+void insert_index(int n,int d)
+{
+    struct node *next = (struct node*)malloc(sizeof(struct node));
+    struct node *i;
+    next->data = d;
+    next->link = NULL;
+    if(head==NULL)
+        head=next;
+    else
+    {
+        struct node *t = head;
+        int count = 0;
+        while(t->link!=NULL)
+        {
+            if(count==n)
+            {
+                i=t;
+            }
+            count++;
+            t=t->link;
+        }
+        if(i==NULL || n>=count)
+            printf("Index too large.!!\n");
+        else
+        {
+            next->link = i->link;
+            i->link = next;
+        }
+    }
+}
+
 int main(){
     insert_front(1);
     insert_end(10);
     insert_end(11);
     insert_middle(10,6);
+    insert_index(2,15);
     print_ll();
     return 0;
 }
